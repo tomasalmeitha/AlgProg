a234f84443ed1c2b68e33512adfb6da91c6ff106
Tomás <almeida.de.tomas@gmail.com>
2023-03-16 16:57:39 +0000

    Update Main.java

--- Ficha3Exe12/src/Main.java
+++ Ficha3Exe12/src/Main.java
@@ -5,9 +5,22 @@ public class Main {
 
         Scanner input = new Scanner(System.in);
 
-        int n1 = 0;
+        int n1, n2, n3;
 
         System.out.print("Insira um número inteiro: ");
-            while(){}
+        n1 = input.nextInt();
+
+
+        while (n1 > 0) {
+            System.out.print("Insira um número inteiro: ");
+            n1 = input.nextInt();
+            n1 = n2;
+            System.out.println(n2);
+        }
+
+        if (n3 < n2) {
+            System.out.println("A sequência está em ordem crescente");
+        }
+        input.close();
     }
 }
